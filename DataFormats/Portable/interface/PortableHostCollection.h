80300c28ac6deb754ff3575127e696346a516c01
Andrea Bocci <andrea.bocci@cern.ch>
2022-12-14 08:52:44 +0100

    Rename static data members following the coding conventions

--- DataFormats/Portable/interface/PortableHostCollection.h
+++ DataFormats/Portable/interface/PortableHostCollection.h
@@ -17,10 +17,11 @@ class PortableHostCollection {
   static_assert(not std::is_same<T3, void>::value or std::is_same<T4, void>::value);
   static_assert(not std::is_same<T2, void>::value or std::is_same<T3, void>::value);
   static_assert(not std::is_same<T1, void>::value or std::is_same<T2, void>::value);
-  template <typename T>
-  static constexpr std::size_t typeCount = CollectionTypeCount<T, T0, T1, T2, T3, T4>;
 
-  static constexpr std::size_t membersCount = CollectionMembersCount<T0, T1, T2, T3, T4>;
+  template <typename T>
+  static constexpr std::size_t count_t_ = CollectionTypeCount<T, T0, T1, T2, T3, T4>;
+
+  static constexpr std::size_t members_ = CollectionMembersCount<T0, T1, T2, T3, T4>;
 
 public:
   using Layout = T;
@@ -33,13 +34,13 @@ public:
   template <typename T>
   static constexpr auto IdxResolver = CollectionIdxResolver<T, T0, T1, T2, T3, T4>;
 
-  using SizesArray = std::array<int32_t, membersCount>;
+  using SizesArray = std::array<int32_t, members_>;
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   using Layout = CollectionTypeResolver<Idx, T0, T1, T2, T3, T4>;
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   using View = typename Layout<Idx>::View;
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   using ConstView = typename Layout<Idx>::ConstView;
 
 private:
@@ -72,7 +73,8 @@ public:
         layout_{buffer_->data(), elements},
         view_{layout_} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
-    assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout::alignment == 0);
+    assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<>::alignment == 0);
+    static_assert(members_ == 1);
   }
 
   template <typename TQueue, typename = std::enable_if_t<alpaka::isQueue<TQueue>>>
@@ -83,35 +85,35 @@ public:
         view_{layout_} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
     assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<>::alignment == 0);
-    static_assert(membersCount == 1);
+    static_assert(members_ == 1);
   }
 
-  static int32_t computeDataSize(const std::array<int32_t, membersCount>& sizes) {
+  static int32_t computeDataSize(const std::array<int32_t, members_>& sizes) {
     int32_t ret = 0;
-    constexpr_for<0, membersCount>([&sizes, &ret](auto i) { ret += Layout<i>::computeDataSize(sizes[i]); });
+    constexpr_for<0, members_>([&sizes, &ret](auto i) { ret += Layout<i>::computeDataSize(sizes[i]); });
     return ret;
   }
 
-  PortableHostCollection(const std::array<int32_t, membersCount>& sizes, alpaka_common::DevHost const& host)
+  PortableHostCollection(const std::array<int32_t, members_>& sizes, alpaka_common::DevHost const& host)
       // allocate pinned host memory associated to the given work queue, accessible by the queue's device
       : buffer_{cms::alpakatools::make_host_buffer<std::byte[]>(computeDataSize(sizes))}, impl_{buffer_->data(), sizes} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
-    constexpr_for<0, membersCount>(
+    constexpr_for<0, members_>(
         [&](auto i) { assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<i>::alignment == 0); });
     constexpr auto alignment = Layout<0>::alignment;
-    constexpr_for<1, membersCount>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
+    constexpr_for<1, members_>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
   }
 
   template <typename TQueue, typename = std::enable_if_t<cms::alpakatools::is_queue_v<TQueue>>>
-  PortableHostCollection(const std::array<int32_t, membersCount>& sizes, TQueue const& queue)
+  PortableHostCollection(const std::array<int32_t, members_>& sizes, TQueue const& queue)
       // allocate pinned host memory associated to the given work queue, accessible by the queue's device
       : buffer_{cms::alpakatools::make_host_buffer<std::byte[]>(queue, computeDataSize(sizes))},
         impl_{buffer_->data(), sizes} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
-    constexpr_for<0, membersCount>(
+    constexpr_for<0, members_>(
         [&](auto i) { assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<i>::alignment == 0); });
     constexpr auto alignment = Layout<0>::alignment;
-    constexpr_for<1, membersCount>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
+    constexpr_for<1, members_>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
   }
 
   // non-copyable
@@ -126,37 +128,37 @@ public:
   ~PortableHostCollection() = default;
 
   // access the View by index
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   View<Idx>& view() {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const& view() const {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const& const_view() const {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   View<Idx>& operator*() {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const& operator*() const {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   View<Idx>* operator->() {
     return &get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const* operator->() const {
     return &get<Idx>().view_;
   }
@@ -205,7 +207,7 @@ public:
   // Extract the sizes array
   SizesArray sizes() const {
     SizesArray ret;
-    constexpr_for<0, membersCount>([&](auto i) { ret[i] = get<i>().layout_.metadata().size(); });
+    constexpr_for<0, members_>([&](auto i) { ret[i] = get<i>().layout_.metadata().size(); });
     return ret;
   }
 
@@ -213,11 +215,11 @@ public:
   static void ROOTReadStreamer(PortableHostCollection* newObj, Layout& layout) {
     newObj->~PortableHostCollection();
     // use the global "host" object returned by cms::alpakatools::host()
-    std::array<int32_t, membersCount> sizes;
-    constexpr_for<0, membersCount>(
+    std::array<int32_t, members_> sizes;
+    constexpr_for<0, members_>(
         [&sizes, &impl](auto i) { sizes[i] = impl.CollectionLeaf<i, Layout<i>>::layout_.metadata().size(); });
     new (newObj) PortableHostCollection(sizes, cms::alpakatools::host());
-    constexpr_for<0, membersCount>([&sizes, &newObj, &impl](auto i) {
+    constexpr_for<0, members_>([&sizes, &newObj, &impl](auto i) {
       newObj->impl_.CollectionLeaf<i, Layout<i>>::layout_.ROOTReadStreamer(impl.CollectionLeaf<i, Layout<i>>::layout_);
     });
   }
