22af6183b440f41fa1def69b18570ddaec784acb
Jonathan Samudio <jonathan_samudio1@baylor.edu>
2023-06-28 17:24:36 +0200

    first addition of clustering parameters ESProducer, needs testing

---
+++ RecoParticleFlow/PFClusterProducerAlpaka/interface/PFClusterParamsAlpakaESData.h
@@ -0,0 +1,17 @@
+#ifndef RecoParticleFlow_PFClusterProducerAlpaka_interface_AlpakaESTestData_h
+#define RecoParticleFlow_PFClusterProducerAlpaka_interface_AlpakaESTestData_h
+
+#include "DataFormats/Portable/interface/PortableHostCollection.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/CopyToDevice.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
+
+#include "RecoParticleFlow/PFClusterProducerAlpaka/interface/PFClusterParamsAlpakaESDataSoA.h"
+
+namespace reco {
+
+  using PFClusterParamsAlpakaESDataHost = PortableHostCollection<PFClusterParamsAlpakaESDataSoA>;
+
+}
+
+#endif
---
+++ RecoParticleFlow/PFClusterProducerAlpaka/interface/alpaka/PFClusterParamsAlpakaESData.h
@@ -0,0 +1,18 @@
+#ifndef RecoParticleFlow_PFClusterProducerAlpaka_interface_alpaka_PFRecHitHBHEParamsAlpakaESData_h
+#define RecoParticleFlow_PFClusterProducerAlpaka_interface_alpaka_PFRecHitHBHEParamsAlpakaESData_h
+
+#include "DataFormats/Portable/interface/alpaka/PortableCollection.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
+
+#include "RecoParticleFlow/PFClusterProducerAlpaka/interface/PFClusterParamsAlpakaESData.h"
+#include "RecoParticleFlow/PFClusterProducerAlpaka/interface/PFClusterParamsAlpakaESDataSoA.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  using PFClusterParamsAlpakaESDataHost = reco::PFClusterParamsAlpakaESDataHost;
+  using PFClusterParamsAlpakaESDataDevice = PortableCollection<reco::PFClusterParamsAlpakaESDataSoA>;
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#endif
