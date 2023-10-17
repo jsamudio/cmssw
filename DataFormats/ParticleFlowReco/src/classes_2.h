337709d8df4e85ad0bbc7b4f13d55860e9346775
Andrea Bocci <andrea.bocci@cern.ch>
2023-10-17 13:48:27 +0200

    Fix DataFormats/ParticleFlowReco dictionaries

--- DataFormats/ParticleFlowReco/src/classes_2.h
+++ DataFormats/ParticleFlowReco/src/classes_2.h
@@ -54,13 +54,3 @@
 #include "DataFormats/ParticleFlowReco/interface/HGCalMultiCluster.h"
 
 #include <map>
-
-#include "DataFormats/ParticleFlowReco/interface/CaloRecHitSoA.h"
-#include "DataFormats/ParticleFlowReco/interface/CaloRecHitHostCollection.h"
-#include "DataFormats/ParticleFlowReco/interface/PFRecHitSoA.h"
-#include "DataFormats/ParticleFlowReco/interface/PFRecHitHostCollection.h"
-
-#include "DataFormats/ParticleFlowReco/interface/PFClusterSoA.h"
-#include "DataFormats/ParticleFlowReco/interface/PFClusterHostCollection.h"
-#include "DataFormats/ParticleFlowReco/interface/PFRecHitFractionSoA.h"
-#include "DataFormats/ParticleFlowReco/interface/PFRecHitFractionHostCollection.h"
