4daf20e2fa18eb7525b9a2d2bc561258ac4792b6
jsamudio <Jonathan_Samudio1@baylor.edu>
2023-11-09 16:56:13 -0600

    code checks

--- RecoParticleFlow/PFClusterProducer/plugins/alpaka/PFClusterSoAProducerKernel.dev.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/alpaka/PFClusterSoAProducerKernel.dev.cc
@@ -33,28 +33,28 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     return (deta * deta + dphi * dphi);
   }
 
-#if defined (__CUDA_ARCH__) or defined (__HIP_DEVICE_COMPILE__)
+#if defined(__CUDA_ARCH__) or defined(__HIP_DEVICE_COMPILE__)
   template <typename TAcc, typename = std::enable_if_t<alpaka::isAccelerator<TAcc>>>
   static __device__ __forceinline__ float atomicMaxF(const TAcc& acc, float* address, float val) {
-      int ret = __float_as_int(*address);
-      while (val > __int_as_float(ret)) {
-        int old = ret;
-        if ((ret = atomicCAS((int*)address, old, __float_as_int(val))) == old)
-          break;
-      }
-      return __int_as_float(ret);
+    int ret = __float_as_int(*address);
+    while (val > __int_as_float(ret)) {
+      int old = ret;
+      if ((ret = atomicCAS((int*)address, old, __float_as_int(val))) == old)
+        break;
+    }
+    return __int_as_float(ret);
   }
 #else
   template <typename TAcc, typename = std::enable_if_t<alpaka::isAccelerator<TAcc>>>
   ALPAKA_FN_ACC ALPAKA_FN_INLINE static float atomicMaxF(const TAcc& acc, float* address, float val) {
-      // CPU implementation uses edm::bit_cast
-      int ret = edm::bit_cast<int>(*address);
-      while (val > edm::bit_cast<float>(ret)) {
-        int old = ret;
-        if ((ret = alpaka::atomicCas(acc, (int*)address, old, edm::bit_cast<int>(val))) == old)
-          break;
-      }
-      return edm::bit_cast<float>(ret);
+    // CPU implementation uses edm::bit_cast
+    int ret = edm::bit_cast<int>(*address);
+    while (val > edm::bit_cast<float>(ret)) {
+      int old = ret;
+      if ((ret = alpaka::atomicCas(acc, (int*)address, old, edm::bit_cast<int>(val))) == old)
+        break;
+    }
+    return edm::bit_cast<float>(ret);
   }
 #endif
 
