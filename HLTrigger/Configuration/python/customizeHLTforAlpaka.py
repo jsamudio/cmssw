7bf8a558deb0856e893d0adf6201581ab8c47bbc
jsamudio <Jonathan_Samudio1@baylor.edu>
2024-02-13 20:04:49 -0600

    Cleanup comments in customize function

--- HLTrigger/Configuration/python/customizeHLTforAlpaka.py
+++ HLTrigger/Configuration/python/customizeHLTforAlpaka.py
@@ -35,19 +35,6 @@ def customizeHLTforAlpakaParticleFlowClustering(process):
             synchronise = cms.untracked.bool(False)
             )
 
-    '''
-    #If needed, set thresholds here? (DB thresholds are bundled with topology)
-    process.hltPFRecHitHCALParamsESProducer = cms.ESProducer("PFRecHitHCALParamsESProducer",
-            energyThresholdsHB = cms.vdouble( 0.1, 0.2, 0.3, 0.3 ),
-            energyThresholdsHE = cms.vdouble( 0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 )
-            )
-    '''
-
-    '''
-    process.hltPFRecHitHCALTopologyESProducer = cms.ESProducer("PFRecHitHCALTopologyESProducer",
-            usePFThresholdsFromDB = cms.bool(True)
-            )
-    '''
     process.hltPFRecHitSoAProducerHCAL = cms.EDProducer("PFRecHitSoAProducerHCAL@alpaka",
             producers = cms.VPSet(
                 cms.PSet(
@@ -63,7 +50,7 @@ def customizeHLTforAlpakaParticleFlowClustering(process):
             src = cms.InputTag("hltPFRecHitSoAProducerHCAL")
             )
     process.particleFlowRecHitHF = cms.EDProducer("PFRecHitProducer",
-    
+
         navigator = cms.PSet(
             name = cms.string("PFRecHitHCALDenseIdNavigator"),
             hcalEnums = cms.vint32(4)
@@ -97,124 +84,13 @@ def customizeHLTforAlpakaParticleFlowClustering(process):
                                      detectorEnum = cms.int32(4))
                           )
                       )
-    
+
               )
         )
       )
 
 )
 
-    #Is there an easier way to do this?
-    '''
-    process.hltPFClusterParamsESProducer = cms.ESProducer("PFClusterParamsESProducer",
-            seedFinder = cms.PSet(
-              nNeighbours = cms.int32(4),
-              thresholdsByDetector = cms.VPSet(
-                cms.PSet(
-                  detector = cms.string('HCAL_BARREL1'),
-                  seedingThreshold = cms.vdouble(
-                    0.125,
-                    0.25,
-                    0.35,
-                    0.35
-                  ),
-                  seedingThresholdPt = cms.double(0)
-                ),
-                cms.PSet(
-                  detector = cms.string('HCAL_ENDCAP'),
-                  seedingThreshold = cms.vdouble(
-                    0.1375,
-                    0.275,
-                    0.275,
-                    0.275,
-                    0.275,
-                    0.275,
-                    0.275
-                  ),
-                  seedingThresholdPt = cms.double(0)
-                )
-              )
-            ),
-            initialClusteringStep = cms.PSet(
-              thresholdsByDetector = cms.VPSet(
-                cms.PSet(
-                  detector = cms.string('HCAL_BARREL1'),
-                  gatheringThreshold = cms.vdouble(
-                    0.1,
-                    0.2,
-                    0.3,
-                    0.3
-                  )
-                ),
-                cms.PSet(
-                  detector = cms.string('HCAL_ENDCAP'),
-                  gatheringThreshold = cms.vdouble(
-                    0.1,
-                    0.2,
-                    0.2,
-                    0.2,
-                    0.2,
-                    0.2,
-                    0.2
-                  )
-                )
-              )
-            ),
-            pfClusterBuilder = cms.PSet(
-              maxIterations = cms.uint32(5),
-              minFracTot = cms.double(1e-20),
-              minFractionToKeep = cms.double(1e-07),
-              excludeOtherSeeds = cms.bool(True),
-              showerSigma = cms.double(10),
-              stoppingTolerance = cms.double(1e-08),
-              recHitEnergyNorms = cms.VPSet(
-                cms.PSet(
-                  detector = cms.string('HCAL_BARREL1'),
-                  recHitEnergyNorm = cms.vdouble(
-                    0.1,
-                    0.2,
-                    0.3,
-                    0.3
-                  )
-                ),
-                cms.PSet(
-                  detector = cms.string('HCAL_ENDCAP'),
-                  recHitEnergyNorm = cms.vdouble(
-                    0.1,
-                    0.2,
-                    0.2,
-                    0.2,
-                    0.2,
-                    0.2,
-                    0.2
-                  )
-                )
-              ),
-              positionCalc = cms.PSet(
-                minFractionInCalc = cms.double(1e-09),
-                minAllowedNormalization = cms.double(1e-09)
-              ),
-              timeResolutionCalcBarrel = cms.PSet(
-                corrTermLowE = cms.double(0),
-                threshLowE = cms.double(6),
-                noiseTerm = cms.double(21.86),
-                constantTermLowE = cms.double(4.24),
-                noiseTermLowE = cms.double(8),
-                threshHighE = cms.double(15),
-                constantTerm = cms.double(2.82)
-              ),
-              timeResolutionCalcEndcap = cms.PSet(
-                corrTermLowE = cms.double(0),
-                threshLowE = cms.double(6),
-                noiseTerm = cms.double(21.86),
-                constantTermLowE = cms.double(4.24),
-                noiseTermLowE = cms.double(8),
-                threshHighE = cms.double(15),
-                constantTerm = cms.double(2.82)
-              )
-            ),
-            )
-    '''
     process.hltPFClusterSoAProducer = cms.EDProducer("PFClusterSoAProducer@alpaka",
             pfRecHits = cms.InputTag("hltPFRecHitSoAProducerHCAL"),
             topology = cms.ESInputTag("pfRecHitHCALTopologyESProducer:"),
@@ -240,7 +116,6 @@ def customizeHLTforAlpakaParticleFlowClustering(process):
             positionReCalc = process.hltParticleFlowClusterHCAL.positionReCalc
             )
 
-    #Define the task (I assume the name has to be the same as the default task)
     process.HLTPFHcalRecHits = cms.Sequence(
             process.hltHBHERecHitToSoA+
             process.hltPFRecHitSoAProducerHCAL+
@@ -259,7 +134,7 @@ def customizeHLTforAlpakaParticleFlowClustering(process):
     #find these Sequences and replace the modules with the Sequence
     def replaceItemsInSequence(process, listProcess, mod):
         for sequence, items in process.sequences.items():
-            containsAll = True 
+            containsAll = True
             for l in listProcess:
                 if(not items.contains(l)):
                     containsAll = False
