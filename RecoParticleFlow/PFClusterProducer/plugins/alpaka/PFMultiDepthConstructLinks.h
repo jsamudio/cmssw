0db2435adcc6a3afe488365637208fcf02ada05c
alexstrel <astrel@fnal.gov>
2025-05-29 12:38:02 -0500

    added more comments in construct links file to help PR review

--- RecoParticleFlow/PFClusterProducer/plugins/alpaka/PFMultiDepthConstructLinks.h
+++ RecoParticleFlow/PFClusterProducer/plugins/alpaka/PFMultiDepthConstructLinks.h
@@ -154,7 +154,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
   ALPAKA_FN_ACC static unsigned int filter_links(TAcc const& acc, 
                                                 const unsigned int mask,
                                                 const unsigned int dst_lane_mask,  
-                                                const T val,
+                                                const T val,//this is a destination lane value (from the "owner" warp)
                                                 const LinkParamKind kind ) {
     // 1. We need to find out the total number of active lanes. 
     unsigned int nLanes = alpaka::popcount(acc, mask);
@@ -183,20 +183,22 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     //    the resulting mask for all initial lanes.
     const unsigned int src_lane_idx = get_ls1b_idx(acc, mask);
     const T test_val = warp::shfl_mask(acc, mask | dst_lane_mask, src_val, src_lane_idx, w_extent);
-    // 7. Return updated link candidate mask:
+    // 7. Return updated link candidate mask: for the "owner" tile this should at least contain the destination lane bit, 
+    //                                        but in the most general case may contain bits for more than one candidate lanes
     const auto filtered_lanes_mask = warp::ballot_mask(acc, mask | dst_lane_mask, (val == test_val));
     //
     return filtered_lanes_mask;
   }
 
   template< typename TAcc, typename = std::enable_if_t<alpaka::isAccelerator<TAcc>>>
-  ALPAKA_FN_HOST_ACC static unsigned int select_link( TAcc const& acc,
+  ALPAKA_FN_HOST_ACC static unsigned int prune_link( TAcc const& acc,
                                                       const unsigned int mask,//excludes the owner lane (corresponding bit set to 0)
                                                       LinkParam& dst_link_params, 
                                                       const LinkParam src_link_params, 
                                                       const unsigned int lane_idx,
                                                       const unsigned int dst_lane_idx,
-                                                      const LinkParamKind kind) {
+                                                      const LinkParamKind kind, 
+						      const bool is_owner_tile = false ) {
     // 1. Create target lane mask:
     const unsigned int dst_lane_mask = (1 << dst_lane_idx);
     // 2. First, we select parameter value for the selection process, based on specified checked value type:
@@ -206,51 +208,51 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     //
     warp::syncWarpThreads_mask(acc, mask | dst_lane_mask);
     //
-    // 3. If we have only one active lane:
+    if (filtered_mask == dst_lane_mask and is_owner_tile) return 0; // the destination lane is the winner, return zero mask
+    //
+    // 4. If we have only one active lane:
     if (((filtered_mask & (filtered_mask - 1)) == 0)) {
       const unsigned int w_extent = alpaka::warp::getSize(acc);
-      // 3.0 Compute the active lane index:
+      // 4.0 Compute the active lane index:
       const unsigned int result_lane_idx = get_ls1b_idx(acc, filtered_mask);
       //
       const unsigned int aggregate_mask = filtered_mask | dst_lane_mask; 
       //
-      // 3.1 Fetch new values from source link:
+      // 4.1 Fetch new values from source link:
       const float dz_ = src_link_params.Get(LinkParamKind::DZ);
-      const auto new_dz = warp::shfl_mask(acc, aggregate_mask, dz_, result_lane_idx, w_extent);
+      const float new_dz = warp::shfl_mask(acc, aggregate_mask, dz_, result_lane_idx, w_extent);
       //
       const float dr_ = src_link_params.Get(LinkParamKind::DR);
-      const auto new_dr = warp::shfl_mask(acc, aggregate_mask, dr_, result_lane_idx, w_extent);
+      const float new_dr = warp::shfl_mask(acc, aggregate_mask, dr_, result_lane_idx, w_extent);
       //
       const float en_ = src_link_params.Get(LinkParamKind::ENERGY); 
-      const auto new_energy = warp::shfl_mask(acc, aggregate_mask, en_, result_lane_idx, w_extent);
+      const float new_energy = warp::shfl_mask(acc, aggregate_mask, en_, result_lane_idx, w_extent);
       //
       const int idx_ = src_link_params.GetIdx();
-      const auto new_idx    = warp::shfl_mask(acc, aggregate_mask, idx_, result_lane_idx, w_extent);
+      const int new_idx    = warp::shfl_mask(acc, aggregate_mask, idx_, result_lane_idx, w_extent);
       //
       int flag = 0;
       //
       if(lane_idx == dst_lane_idx) {
-        const auto old_dz = dst_link_params.Get(LinkParamKind::DZ); 
+        const float old_dz = dst_link_params.Get(LinkParamKind::DZ); 
         //
         if ( old_dz > new_dz ) { 
           flag =  1;
         } else if ( old_dz == new_dz ) {
-          const auto old_dr = dst_link_params.Get(LinkParamKind::DR);
+          const float old_dr = dst_link_params.Get(LinkParamKind::DR);
           if ( old_dr > new_dr ) { 
             flag =  1;
           } else if ( old_dr == new_dr ) {
-            const auto old_energy = dst_link_params.Get(LinkParamKind::ENERGY);
+            const float old_energy = dst_link_params.Get(LinkParamKind::ENERGY);
             if ( old_energy < new_energy ) flag = 1;
           }
         }
         if (flag == 1) dst_link_params.Set(new_idx,new_dz,new_dr,new_energy);
       }
-      //destribute control flag among active lanes:
+      //
       warp::syncWarpThreads_mask(acc, mask | dst_lane_mask);
-      //const int control_flag = warp::shfl_mask(acc, mask | dst_lane_mask, flag, result_lane_idx, w_extent);
-      // reset mask (uniform operation):
-      //if (control_flag == 1) return 0x0;
-      return 0x0;
+
+      return 0;
     } 
     return filtered_mask;
   }
@@ -308,11 +310,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
             // but it may have up to nCluster-1 non-zero entries per row. Obviously, the matrix has zeros on the diagonal 
             // (self-linking is excluded).
             for (unsigned int tile = 0; tile < cluster_tiles; tile++) {            
-              // We call a cluster tile as the 'proper' tile 
+              // We call a cluster tile as the 'owner' tile 
               // if the lane index of each thread coincide with the destination cluster index 
               // (but in fact we compare warp index with tile index):
               const bool is_owner_tile = (warp_idx + group * nBlocks) == (tile + group * cluster_tiles); 
-              // A destination cluster params, for 'non-proper' tile load cluster data again:
+              // A destination cluster params, for 'non-owner' tile load cluster data again:
               const auto src_idx = (tile * cluster_tile_size + lane_idx) + group * nBlocks;
               //
               const auto src_cluster_params = is_owner_tile ? ClusterParam(dst_cluster_params) : ClusterParam(mdpfClusteringVars[src_idx]);
@@ -342,16 +344,16 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
                 // Note that lane with id equal to iter_lane_idx must be always active, since it's responsible for storing 
                 // result.
                 // 6. Skip if : 
-                //            1)  there are no active lanes in the filtered mask (then all lanes must skip);
-                if (filtered_lanes_mask == 0x0)  continue;
+                //            6.1)  there are no active lanes in the filtered mask (then all lanes must skip);
+                if (filtered_lanes_mask == 0)  continue;
                 // here and in some places below we need to combine the destination lane 
                 // with the filtered mask to avoid undefined behavior:
                 filtered_lanes_mask |= dst_lane_mask;
-                //            2)  there are unwanted (inactive) lanes but keep the destination (target) lane active;
+                //            6.2)  there are unwanted (inactive) lanes but keep the destination (target) lane active;
                 if ( is_active_lane(filtered_lanes_mask, lane_idx) == false ) continue; 
 
                 warp::syncWarpThreads_mask(acc, filtered_lanes_mask);
-                // WARNING: from this point only lanes selected in the filtered_lanes_mask[0] plus destination lane are active in iteration. 
+                // WARNING: from this point only lanes selected in the filtered_lanes_mask plus destination lane are active in iteration. 
                 const float eta = src_cluster_params.Get<ClusterParamKind::ETA>();
 		const float src_eta = warp::shfl_mask(acc, filtered_lanes_mask, eta, iter_lane_idx, w_extent);
 		
@@ -384,8 +386,8 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 		const float energy = src_cluster_params.Get<ClusterParamKind::ENERGY>();
                 const float src_energy = warp::shfl_mask(acc, filtered_lanes_mask, energy, iter_lane_idx, w_extent);  
                 //
-                // Now start inter-warp pruning:
-                // Create warp-local link params (with the latest filtered lane mask);
+                // 7. Now start inter-warp pruning:
+                // 7.1 Create warp-local link params (with the latest filtered lane mask);
                 next_filtered_lanes_mask = is_owner_tile ? filtered_lanes_mask ^ dst_lane_mask : filtered_lanes_mask;
 
 		LinkParam candidate_link_params;
@@ -399,16 +401,18 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
   			candidate_link_params = LinkParam(idx.global);
 		}
 
+		// 7.2 Check 3 parameters (dZ, dR, energy) to prune the candidate links:
                 CMS_UNROLL_LOOP
                 for (unsigned int k = 0; k < 3; k++) {
-                  // Check 3 parameters to select the link:
+                  // Reset filtered link mask:
                   filtered_lanes_mask = next_filtered_lanes_mask;
                   //
                   warp::syncWarpThreads_mask(acc, filtered_lanes_mask | dst_lane_mask);
                   //
-                  next_filtered_lanes_mask = select_link(acc, filtered_lanes_mask, selected_link_params, candidate_link_params, lane_idx, iter_lane_idx, param_kinds[k]);
+                  next_filtered_lanes_mask = prune_link(acc, filtered_lanes_mask, selected_link_params, 
+							candidate_link_params, lane_idx, iter_lane_idx, param_kinds[k], is_owner_tile);
                   //
-                  if ( next_filtered_lanes_mask == 0x0 ) continue;//exit loop for all lanes if the new mask is empty
+                  if ( next_filtered_lanes_mask == 0 ) continue;//exit for all active lanes if the new mask is empty
                   else if ( is_active_lane(next_filtered_lanes_mask | dst_lane_mask, lane_idx) == false ) continue; // exit loop for filtered lanes only
                 }                                    
                 //
