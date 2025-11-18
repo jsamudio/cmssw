61f8d8dbeb0c7fbf2e2faaacfc4e90054e8241bb
jsamudio <Jonathan_Samudio1@baylor.edu>
2025-11-18 14:02:51 -0600

    Add comparison for Alpaka multi-depth clustering in .423 workflow

--- Validation/RecoParticleFlow/python/PFClusterValidation_cff.py
+++ Validation/RecoParticleFlow/python/PFClusterValidation_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoParticleFlow.pfClusterValidation_cfi import pfClusterValidation
 from Validation.RecoParticleFlow.pfCaloGPUComparisonTask_cfi import pfClusterHBHEOnlyAlpakaComparison, pfClusterHBHEAlpakaComparison
 
+from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 pfClusterValidationSequence = cms.Sequence( pfClusterValidation )
 
 pfClusterAlpakaComparisonSequence = cms.Sequence( pfClusterHBHEAlpakaComparison )
@@ -10,6 +11,13 @@ pfClusterCaloOnlyValidation = pfClusterValidation.clone(
     pflowClusterHCAL = 'particleFlowClusterHCALOnlyLegacy'
 )
 
+pfClusterHCALOnlyAlpakaComparison = DQMEDAnalyzer("PFMultiClusCompare",
+                                                    pfClusterToken_ref = cms.untracked.InputTag('particleFlowClusterHCALOnlyLegacy'),
+                                                    pfClusterToken_target = cms.untracked.InputTag('particleFlowClusterHCALOnly'),
+                                                    pfCaloGPUCompDir = cms.untracked.string("pfClusterHCALAlpakaV")
+)
+
+
 pfClusterCaloOnlyValidationSequence = cms.Sequence( pfClusterCaloOnlyValidation )
 
-pfClusterHBHEOnlyAlpakaComparisonSequence = cms.Sequence( pfClusterHBHEOnlyAlpakaComparison )
+pfClusterHBHEOnlyAlpakaComparisonSequence = cms.Sequence( pfClusterHBHEOnlyAlpakaComparison + pfClusterHCALOnlyAlpakaComparison )
